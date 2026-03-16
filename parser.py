1b1835871d6d92f22377181216ea9aab5ec75e23
sevcevo <sevcevo@uzlabina.student.cz>
2026-03-16 11:11:48 +0100

    start

---
+++ parser.py
@@ -0,0 +1,7 @@
+def parse_file(filepath):
+    with open(filepath) as f:
+        lines = f.readlines()
+        parsed_lines = []
+        for line in lines:
+            parsed_lines.append(line)
+
